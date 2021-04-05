5d5ee397efd239cd98aedf90258eb04a792104dc
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 15:09:28 -0500

    adds switch to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return(
       <Router>
         <Nav />
-          <div className="App">
-            <h1>Testing</h1>
-          </div>
-        </Router>
+        <Switch>
+          <Route exact path="/" component = { Home } />
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
 }
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Pro Bono or Low Cost Legal Representation</h2>
+            </div>
+        )
+    }
+}
+
+export default Home
