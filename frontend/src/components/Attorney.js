45051a6fabd82409d5c2c8800f8805dbd9d9cdab
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 14:57:42 -0500

    adds components

---
+++ frontend/src/components/Attorney.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Attorney extends Component {
+    render() {
+        return (
+            <div>
+                Attorney
+            </div>
+        )
+    }
+}
+
+export default Attorney
