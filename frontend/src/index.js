78b9081f439e8b0c9ac45f38cc66ed7a4ff98613
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 14:42:26 -0500

    finishes importing all redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import attorneysReducer from './reducers/attorneysReducer'
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(attorneysReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}></Provider>
     <App />
+  <Provider />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- frontend/src/reducers/attorneysReducer.js
+++ frontend/src/reducers/attorneysReducer.js
@@ -1,6 +1,13 @@
+const initialState = {
+    attorneys: [],
+    loading: true
+}
+
 const attorneysReducer = (state, action) => {
     switch(action.type) {
         default: 
             return state;
     }
 }
+
+export default attorneysReducer;
