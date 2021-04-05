0f40f2a37442ddf969df918d37be9ef6df8cccd3
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 15:02:24 -0500

    imports navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Testing</h1>
-      </div>
+    return(
+      <Router>
+        <Nav />
+          <div className="App">
+            <h1>Testing</h1>
+          </div>
+        </Router>
     );
   }
 }
