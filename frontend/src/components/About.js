4cd3626cdc132153d483505de65fd85e4a22cbc4
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 15:25:50 -0500

    adds about page to navbar

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,7 +3,9 @@ class About extends Component {
     render() {
         return (
             <div>
-                About Page!
+                <h3>Purpose Statement:</h3>
+                
+                <p>This service is meant for those who are looking for legal representation but do not have the luxury of hiring a high dollar attorney. We strive to connect you with licensed, ethical attorneys and other legal professionals who will work or advise on your case free of charge or for a drastically reduced rate.</p>
             </div>
         )
     }
