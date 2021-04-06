e276095e31dea9af10991d84380568e6eeeb9468
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-06 08:41:20 -0500

    create form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,7 +4,19 @@ class Form extends Component {
     render() {
         return (
             <div>
-                Form
+                <h3>Apply to Offer Your Services</h3>
+                <form>
+                    <div>
+                        <label>Your Name: </label>
+                        <input type="text" />
+                        <br/>
+                        <label>Your Location: </label>
+                        <input type="text" />
+                        <br />
+                        <textarea cols="30" rows="3" placeholder="A brief description of the services you offer..."></textarea>
+                    </div>
+                    <input type="submit" value="Register" />
+                </form>
             </div>
         )
     }
