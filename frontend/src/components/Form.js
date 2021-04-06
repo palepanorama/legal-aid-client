dc38762bf8f7b1918868ae826b143ddd4004e584
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-06 08:46:43 -0500

    adds state to form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+    state = {
+        name: "",
+        location: "",
+        description: ""
+    }
+
     render() {
         return (
             <div>
                 <h3>Apply to Offer Your Services</h3>
                 <form>
                     <div>
-                        <label>Your Name: </label>
-                        <input type="text" />
+                        <label htmlFor="name">Your Name: </label>
+                        <input type="text" id="name" value={ this.state.name } />
                         <br/>
-                        <label>Your Location: </label>
-                        <input type="text" />
+                        <label htmlFor="location">Your Location: </label>
+                        <input type="text" id="location" value={ this.state.location } />
                         <br />
-                        <textarea cols="30" rows="3" placeholder="A brief description of the services you offer..."></textarea>
+                        <p><label htmlFor="description">Description: </label></p>
+                        <textarea id="description" cols="30" rows="3" value={ this.state.description } placeholder="A brief description of the services you offer..."></textarea>
                     </div>
                     <input type="submit" value="Register" />
                 </form>
